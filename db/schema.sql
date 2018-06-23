fdd99a4a318dd17bd555073ef6772dbf1502da00
Seirradawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-06-23 10:11:08 -0400

    updated burger img

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT (10) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (100) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN NOT NULL,
     date TIMESTAMP,
     PRIMARY KEY (id)
 );
